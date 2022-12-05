17ffb9f712d0edc7dbcb364454e05b8899c652ab
Rangrang Zheng <zhengr@hawaii.edu>
2022-12-05 01:10:46 -1000

    changes of csv files

--- testtt.py
+++ testtt.py
@@ -86,6 +86,8 @@ gc = GeneratorClusters(pudl_engine, pudl_out, pg_engine, scenario_settings[2020]
 
 
 all_gen = gc.create_all_generators()
+all_gen["Resource"] = all_gen["Resource"].str.rstrip("_")
+all_gen["technology"] = all_gen["technology"].str.rstrip("_")
 all_gen["plant_id_eia"] = all_gen["plant_id_eia"].astype("Int64")
 existing_gen = all_gen.loc[
     all_gen["plant_id_eia"].notna(), :
@@ -196,6 +198,8 @@ pg_build = plant_gen_id(pg_build)
 # add in the plant+pudl id to the all_gen and pg_build tables (plant_id_eia + unit_pudl_id)
 # pg_build = plant_pudl_id(pg_build)
 all_gen = plant_pudl_id(all_gen)
+######################################################################################
+######################################################################################
 
 if settings.get("cogen_tech"):
     cogen_tech = settings["cogen_tech"]
@@ -608,93 +612,95 @@ complete_gens["gen_energy_source"] = complete_gens["technology"].apply(
     lambda x: energy_tech[x]
 )
 
+##################################################################################
+##################################################################################
 
-# load_curves = make_final_load_curves(pg_engine, scenario_settings[2020]["p1"])
-# timeseries_df = timeseries(
-#     load_curves,
-#     max_weight=20.2778,
-#     avg_weight=283.8889,
-#     ts_duration_of_tp=4,
-#     ts_num_tps=6,
-# )
-# timeseries_dates = timeseries_df["timeseries"].to_list()
-# timestamp_interval = [
-#     "00",
-#     "04",
-#     "08",
-#     "12",
-#     "16",
-#     "20",
-# ]  # should align with ts_duration_of_tp and ts_num_tps
-# timepoints_df = timepoints_table(timeseries_dates, timestamp_interval)
-# # create lists and dictionary for later use
-# timepoints_timestamp = timepoints_df["timestamp"].to_list()  # timestamp list
-# timepoints_tp_id = timepoints_df["timepoint_id"].to_list()  # timepoint_id list
-# timepoints_dict = dict(
-#     zip(timepoints_timestamp, timepoints_tp_id)
-# )  # {timestamp: timepoint_id}
+load_curves = make_final_load_curves(pg_engine, scenario_settings[2020]["p1"])
+timeseries_df = timeseries(
+    load_curves,
+    max_weight=20.2778,
+    avg_weight=283.8889,
+    ts_duration_of_tp=4,
+    ts_num_tps=6,
+)
+timeseries_dates = timeseries_df["timeseries"].to_list()
+timestamp_interval = [
+    "00",
+    "04",
+    "08",
+    "12",
+    "16",
+    "20",
+]  # should align with ts_duration_of_tp and ts_num_tps
+timepoints_df = timepoints_table(timeseries_dates, timestamp_interval)
+# create lists and dictionary for later use
+timepoints_timestamp = timepoints_df["timestamp"].to_list()  # timestamp list
+timepoints_tp_id = timepoints_df["timepoint_id"].to_list()  # timepoint_id list
+timepoints_dict = dict(
+    zip(timepoints_timestamp, timepoints_tp_id)
+)  # {timestamp: timepoint_id}
 
-# period_list = ["2020", "2030", "2040", "2050"]
-# loads, loads_with_year_hour = loads_table(
-#     load_curves, timepoints_timestamp, timepoints_dict, period_list
-# )
-# # for fuel_cost and regional_fuel_market issue
-# dummy_df = pd.DataFrame({"TIMEPOINT": timepoints_tp_id})
-# dummy_df.insert(0, "LOAD_ZONE", "loadzone")
-# dummy_df.insert(2, "zone_demand_mw", 0)
-# loads = loads.append(dummy_df)
+period_list = ["2020", "2030", "2040", "2050"]
+loads, loads_with_year_hour = loads_table(
+    load_curves, timepoints_timestamp, timepoints_dict, period_list
+)
+# for fuel_cost and regional_fuel_market issue
+dummy_df = pd.DataFrame({"TIMEPOINT": timepoints_tp_id})
+dummy_df.insert(0, "LOAD_ZONE", "loadzone")
+dummy_df.insert(2, "zone_demand_mw", 0)
+loads = loads.append(dummy_df)
 
-# year_hour = loads_with_year_hour["year_hour"].to_list()
-# all_gen_variability = make_generator_variability(all_gen)
+year_hour = loads_with_year_hour["year_hour"].to_list()
+all_gen_variability = make_generator_variability(all_gen)
 
-# v_capacity_factors = all_gen_variability.copy().transpose()
-# v_capacity_factors["GENERATION_PROJECT"] = v_capacity_factors.index
-# v_c_f = v_capacity_factors.melt(
-#     id_vars="GENERATION_PROJECT",
-#     var_name="year_hour",
-#     value_name="gen_max_capacity_factor",
-# )
-# # reduce variability to just the hours of the year that have a timepoint
-# v_c_f = v_c_f[v_c_f["year_hour"].isin(year_hour)]
+v_capacity_factors = all_gen_variability.copy().transpose()
+v_capacity_factors["GENERATION_PROJECT"] = v_capacity_factors.index
+v_c_f = v_capacity_factors.melt(
+    id_vars="GENERATION_PROJECT",
+    var_name="year_hour",
+    value_name="gen_max_capacity_factor",
+)
+# reduce variability to just the hours of the year that have a timepoint
+v_c_f = v_c_f[v_c_f["year_hour"].isin(year_hour)]
 
-# mod_vcf = v_c_f.copy()
-# # get the dates from hour of the year
-# start = pd.to_datetime("2021-01-01 0:00")  # 2020 is a leap year
-# mod_vcf["date"] = mod_vcf["year_hour"].apply(
-#     lambda x: start + pd.to_timedelta(x, unit="H")
-# )
-# mod_vcf["reformat"] = mod_vcf["date"].apply(lambda x: x.strftime("%Y%m%d%H"))
-# mod_vcf["reformat"] = mod_vcf["reformat"].astype(str)
-# date_list = mod_vcf["reformat"].to_list()
-# # change 2021 to correct period year/decade
-# # to get the timestamp
-# updated_dates20 = ["2020" + x[4:] for x in date_list]
-# updated_dates30 = ["2030" + x[4:] for x in date_list]
-# updated_dates40 = ["2040" + x[4:] for x in date_list]
-# updated_dates50 = ["2050" + x[4:] for x in date_list]
-# mod_vcf1 = mod_vcf.copy()
-# mod_vcf2 = mod_vcf.copy()
-# mod_vcf3 = mod_vcf.copy()
-# mod_vcf4 = mod_vcf.copy()
-# mod_vcf1["timestamp"] = updated_dates20
-# mod_vcf2["timestamp"] = updated_dates30
-# mod_vcf3["timestamp"] = updated_dates40
-# mod_vcf4["timestamp"] = updated_dates50
-# # go from timestamp to timepoint
-# mod_vcf1["timepoint"] = mod_vcf1["timestamp"].apply(lambda x: timepoints_dict[x])
-# mod_vcf2["timepoint"] = mod_vcf2["timestamp"].apply(lambda x: timepoints_dict[x])
-# mod_vcf3["timepoint"] = mod_vcf3["timestamp"].apply(lambda x: timepoints_dict[x])
-# mod_vcf4["timepoint"] = mod_vcf4["timestamp"].apply(lambda x: timepoints_dict[x])
-# # get final columns
-# mod_vcf1.drop(["year_hour", "date", "reformat", "timestamp"], axis=1, inplace=True)
-# mod_vcf2.drop(["year_hour", "date", "reformat", "timestamp"], axis=1, inplace=True)
-# mod_vcf3.drop(["year_hour", "date", "reformat", "timestamp"], axis=1, inplace=True)
-# mod_vcf4.drop(["year_hour", "date", "reformat", "timestamp"], axis=1, inplace=True)
-# # bring all decades together
-# var_cap_fac = pd.concat([mod_vcf1, mod_vcf2, mod_vcf3, mod_vcf4], ignore_index=True)
+mod_vcf = v_c_f.copy()
+# get the dates from hour of the year
+start = pd.to_datetime("2021-01-01 0:00")  # 2020 is a leap year
+mod_vcf["date"] = mod_vcf["year_hour"].apply(
+    lambda x: start + pd.to_timedelta(x, unit="H")
+)
+mod_vcf["reformat"] = mod_vcf["date"].apply(lambda x: x.strftime("%Y%m%d%H"))
+mod_vcf["reformat"] = mod_vcf["reformat"].astype(str)
+date_list = mod_vcf["reformat"].to_list()
+# change 2021 to correct period year/decade
+# to get the timestamp
+updated_dates20 = ["2020" + x[4:] for x in date_list]
+updated_dates30 = ["2030" + x[4:] for x in date_list]
+updated_dates40 = ["2040" + x[4:] for x in date_list]
+updated_dates50 = ["2050" + x[4:] for x in date_list]
+mod_vcf1 = mod_vcf.copy()
+mod_vcf2 = mod_vcf.copy()
+mod_vcf3 = mod_vcf.copy()
+mod_vcf4 = mod_vcf.copy()
+mod_vcf1["timestamp"] = updated_dates20
+mod_vcf2["timestamp"] = updated_dates30
+mod_vcf3["timestamp"] = updated_dates40
+mod_vcf4["timestamp"] = updated_dates50
+# go from timestamp to timepoint
+mod_vcf1["timepoint"] = mod_vcf1["timestamp"].apply(lambda x: timepoints_dict[x])
+mod_vcf2["timepoint"] = mod_vcf2["timestamp"].apply(lambda x: timepoints_dict[x])
+mod_vcf3["timepoint"] = mod_vcf3["timestamp"].apply(lambda x: timepoints_dict[x])
+mod_vcf4["timepoint"] = mod_vcf4["timestamp"].apply(lambda x: timepoints_dict[x])
+# get final columns
+mod_vcf1.drop(["year_hour", "date", "reformat", "timestamp"], axis=1, inplace=True)
+mod_vcf2.drop(["year_hour", "date", "reformat", "timestamp"], axis=1, inplace=True)
+mod_vcf3.drop(["year_hour", "date", "reformat", "timestamp"], axis=1, inplace=True)
+mod_vcf4.drop(["year_hour", "date", "reformat", "timestamp"], axis=1, inplace=True)
+# bring all decades together
+var_cap_fac = pd.concat([mod_vcf1, mod_vcf2, mod_vcf3, mod_vcf4], ignore_index=True)
 
-# # only get all_gen plants that are wind or solar
-# technology = all_gen["technology"].to_list()
+# only get all_gen plants that are wind or solar
+technology = all_gen["technology"].to_list()
 
 
 # def Filter(list1, list2):
@@ -703,50 +709,55 @@ complete_gens["gen_energy_source"] = complete_gens["technology"].apply(
 
 # wind_solar = set(Filter(technology, ["Wind", "Solar"]))
 # all_gen.loc[all_gen["technology"].isin(wind_solar), "gen_is_variable"] = True
-# all_gen = all_gen[all_gen["gen_is_variable"] == True]
+all_gen["temp_id"] = all_gen.index
+all_gen.loc[all_gen["technology"].str.contains("Wind"), "gen_is_variable"] = True
+all_gen.loc[all_gen["technology"].str.contains("Solar"), "gen_is_variable"] = True
+all_gen.loc[all_gen["technology"].str.contains("PV"), "gen_is_variable"] = True
 
-# # get the correct GENERATION_PROJECT instead of region_resource_cluster from variability table
-# # all_gen = all_gen.copy()
-# # all_gen["region_resource_cluster"] = (
-# #     all_gen["region"]
-# #     + "_"
-# #     + all_gen["Resource"]
-# #     + "_"
-# #     + all_gen["cluster"].astype(str)
-# # )
-# # all_gen["gen_id"] = all_gen.index
-# # all_gen_convert = dict(
-# #     zip(all_gen["region_resource_cluster"].to_list(), all_gen["gen_id"].to_list())
-# # )
+all_gen = all_gen[all_gen["gen_is_variable"] == True]
 
-# # reg_res_cl = all_gen["region_resource_cluster"].to_list()
-# reg_res_cl = all_gen["index"].to_list()
-# all(isinstance(n, float) for n in reg_res_cl)
+# get the correct GENERATION_PROJECT instead of region_resource_cluster from variability table
+# all_gen = all_gen.copy()
+# all_gen["region_resource_cluster"] = (
+#     all_gen["region"]
+#     + "_"
+#     + all_gen["Resource"]
+#     + "_"
+#     + all_gen["cluster"].astype(str)
+# )
+# all_gen["gen_id"] = all_gen.index
+# all_gen_convert = dict(
+#     zip(all_gen["region_resource_cluster"].to_list(), all_gen["gen_id"].to_list())
+# )
 
-# import math
+# reg_res_cl = all_gen["region_resource_cluster"].to_list()
+reg_res_cl = all_gen["temp_id"].to_list()
+all(isinstance(n, float) for n in reg_res_cl)
 
-# reg_res_cl_copy = [str(i) for i in reg_res_cl]
-# all(isinstance(n, str) for n in reg_res_cl_copy)
+import math
+
+reg_res_cl_copy = [str(i) for i in reg_res_cl]
+all(isinstance(n, str) for n in reg_res_cl_copy)
 
 # reg_res_cl = [i[0:-2] for i in reg_res_cl_copy]
 
 # var_cap_fac["GENERATION_PROJECT"] = var_cap_fac["GENERATION_PROJECT"] + 1
 
-# var_cap_fac["GENERATION_PROJECT"] = [int(i) for i in var_cap_fac["GENERATION_PROJECT"]]
+var_cap_fac["GENERATION_PROJECT"] = [int(i) for i in var_cap_fac["GENERATION_PROJECT"]]
 
-# all(isinstance(n, str) for n in var_cap_fac["GENERATION_PROJECT"])
+all(isinstance(n, str) for n in var_cap_fac["GENERATION_PROJECT"])
 
-# var_cap_fac = var_cap_fac[var_cap_fac["GENERATION_PROJECT"].isin(reg_res_cl)]
+var_cap_fac = var_cap_fac[var_cap_fac["GENERATION_PROJECT"].isin(reg_res_cl_copy)]
 
-# # var_cap_fac["GENERATION_PROJECT"] = var_cap_fac["GENERATION_PROJECT"].apply(
-# #     lambda x: all_gen_convert[x]
-# # )
-# # filter to final columns
-# var_cap_fac = var_cap_fac[
-#     ["GENERATION_PROJECT", "timepoint", "gen_max_capacity_factor"]
-# ]
-# var_cap_fac["GENERATION_PROJECT"] = var_cap_fac["GENERATION_PROJECT"] + 1
-
-# vcf = variable_capacity_factors_table(
-#     all_gen_variability, year_hour, timepoints_dict, all_gen
+# var_cap_fac["GENERATION_PROJECT"] = var_cap_fac["GENERATION_PROJECT"].apply(
+#     lambda x: all_gen_convert[x]
 # )
+# filter to final columns
+var_cap_fac = var_cap_fac[
+    ["GENERATION_PROJECT", "timepoint", "gen_max_capacity_factor"]
+]
+var_cap_fac["GENERATION_PROJECT"] = var_cap_fac["GENERATION_PROJECT"] + 1
+
+vcf = variable_capacity_factors_table(
+    all_gen_variability, year_hour, timepoints_dict, all_gen
+)
